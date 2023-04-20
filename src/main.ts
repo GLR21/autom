b64a5ab51ef7d234e8e7773f8ad900802766e8b8
GLR21 <gabriellange.ramos@gmail.com>
2023-04-19 23:17:14 -0300

    Ajuste para tratar erro CORS

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import 'dotenv/config'
 import express from 'express';
 import Logging from './util/Logging';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT;
 const host = process.env.HOST;
 
 app.use( express.json() );
+
+const corsOptions = 
+{
+	origin: 'http://localhost:3000',
+	optionsSuccessStatus: 200,
+	credentials: true
+}
+
+app.use( cors( corsOptions ) );
 
 app.listen( port, () => 
 {	
